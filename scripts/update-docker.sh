12af01bf26f0cd215b6e6fea56b4defbc90fa982
John Comeau <jc@unternet.net>
2021-08-09 12:46:59 -0700

    added sudo. runs now but doesn't update

--- scripts/update-docker.sh
+++ scripts/update-docker.sh
@@ -4,16 +4,16 @@ set -e -u
 CONTAINER=termux-package-builder
 IMAGE=termux/package-builder
 
-docker pull $IMAGE
+sudo docker pull $IMAGE
 
-LATEST=$(docker inspect --format "{{.Id}}" $IMAGE)
-RUNNING=$(docker inspect --format "{{.Image}}" $CONTAINER)
+LATEST=$(sudo docker inspect --format "{{.Id}}" $IMAGE)
+RUNNING=$(sudo docker inspect --format "{{.Image}}" $CONTAINER)
 
 if [ $LATEST = $RUNNING ]; then
 	echo "Image '$IMAGE' used in the container '$CONTAINER' is already up to date"
 else
 	echo "Image '$IMAGE' used in the container '$CONTAINER' has been updated - removing the outdated container"
-	docker stop $CONTAINER
-	docker rm -f $CONTAINER
+	sudo docker stop $CONTAINER
+	sudo docker rm -f $CONTAINER
 fi
 
