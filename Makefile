8a6452cc3a9dff710f5abf0eea3c8bbc5b30dbbe
John Comeau <jc@unternet.net>
2021-08-08 19:12:31 -0700

    nowhere near working yet

--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
-all: build-all.sh
-	./$<
+# https://github.com/termux/termux-packages/wiki/Build-environment
+all: scripts/run-docker.sh
+	$< ./build-all.sh
+/usr/bin/docker:
+	sudo apt install docker.io
+sdk:
+	./scripts/setup-android-sdk.sh
--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -17,9 +17,9 @@ USER=builder
 
 echo "Running container '$CONTAINER_NAME' from image '$TERMUX_BUILDER_IMAGE_NAME'..."
 
-docker start $CONTAINER_NAME > /dev/null 2> /dev/null || {
+sudo docker start $CONTAINER_NAME > /dev/null 2> /dev/null || {
 	echo "Creating new container..."
-	docker run \
+	sudo docker run \
 		--detach \
 		--name $CONTAINER_NAME \
 		--volume $REPOROOT:$HOME/termux-packages \
@@ -29,18 +29,18 @@ docker start $CONTAINER_NAME > /dev/null 2> /dev/null || {
 	if [ $(id -u) -ne 1000 -a $(id -u) -ne 0 ]
 	then
 		echo "Changed builder uid/gid... (this may take a while)"
-		docker exec --tty $CONTAINER_NAME sudo chown -R $(id -u) $HOME
-		docker exec --tty $CONTAINER_NAME sudo chown -R $(id -u) /data
-		docker exec --tty $CONTAINER_NAME sudo usermod -u $(id -u) builder
-		docker exec --tty $CONTAINER_NAME sudo groupmod -g $(id -g) builder
+		sudo docker exec --tty $CONTAINER_NAME sudo chown -R $(id -u) $HOME
+		sudo docker exec --tty $CONTAINER_NAME sudo chown -R $(id -u) /data
+		sudo docker exec --tty $CONTAINER_NAME sudo usermod -u $(id -u) builder
+		sudo docker exec --tty $CONTAINER_NAME sudo groupmod -g $(id -g) builder
 	fi
     fi
 }
 
 if [ "$#" -eq  "0" ]; then
-	docker exec --interactive --tty $CONTAINER_NAME bash
+	sudo docker exec --interactive --tty $CONTAINER_NAME bash
 else
-	docker exec --interactive --tty $CONTAINER_NAME $@
+	sudo docker exec --interactive --tty $CONTAINER_NAME $@
 fi
 
 
